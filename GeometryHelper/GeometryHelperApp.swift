2d82da56c1e42e5b421596b3e35a9a76edccaad8
Yukun Xie <yxie@lcs.on.ca>
2023-11-16 19:55:57 -0500

    Initial Commit

---
+++ GeometryHelper/GeometryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryHelperApp.swift
+//  GeometryHelper
+//
+//  Created by Yukun Xie on 2023/11/16.
+//
+
+import SwiftUI
+
+@main
+struct GeometryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeometryHelperUITests/GeometryHelperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GeometryHelperUITestsLaunchTests.swift
+//  GeometryHelperUITests
+//
+//  Created by Yukun Xie on 2023/11/16.
+//
+
+import XCTest
+
+final class GeometryHelperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
