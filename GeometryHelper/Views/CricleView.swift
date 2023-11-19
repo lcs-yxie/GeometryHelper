3eafefdaf3ebf09d793fe8e289a3de49f30cc70e
Yukun Xie <yxie@lcs.on.ca>
2023-11-18 21:10:02 -0500

    Organized project by adding Views and Model groups and a new view file named CircleView.

---
+++ GeometryHelper/Views/CricleView.swift
@@ -0,0 +1,18 @@
+//
+//  CricleView.swift
+//  GeometryHelper
+//
+//  Created by Yukun Xie on 2023/11/18.
+//
+
+import SwiftUI
+
+struct CricleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CricleView()
+}
